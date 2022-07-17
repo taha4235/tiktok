d9a7f567994b160a785813b7205bee9c6be199d3
taha kourani <tahakourani88@gmail.com>
2022-07-17 14:50:34 +0300

    first web file

---
+++ index.js
@@ -0,0 +1,10 @@
+/*const videos = document.querySelectorAll('video')
+for (const video of videos) {
+    videos.addEventListener('click', function() {
+        if (video.pause) {
+            video.play()
+        } else {
+            video.pause
+        }
+    })
+}*/
